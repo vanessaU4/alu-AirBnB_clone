f5f2604d81b5d23238737cb56ccf504bc4d8c17f
vumugwaneza <116736307+vanessaU4@users.noreply.github.com>
2023-10-04 09:10:15 +0000

    all

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# alu-AirBnB_clone
+alu airbnb
---
+++ models/amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module documentation
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Amenity class
+    """
+    name = ""
+    
